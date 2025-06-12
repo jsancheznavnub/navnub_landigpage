886702936d1a5c7294da55ab8ec8faf1ebb50109
Jonathan Johan Marquez Sanchez <cheka993jo@gmail.com>
2025-06-12 18:41:41 +0000

    agrega la imagen en el componente de App Móvil para Logística Apps Web y

--- src/app/[lang]/casos-de-exito/page.tsx
+++ src/app/[lang]/casos-de-exito/page.tsx
@@ -98,6 +98,14 @@ const getProjects = async (dscp: Dictionary['successCasesPage']): Promise<Projec
             currentImageUrl = signedUrl;
           }
         }
+      } else if (p.id === "5") {
+        const imageKey = process.env.NEXT_PUBLIC_SUCCESS_CASE_LOGISTICS_APP_IMAGE_KEY;
+        if (imageKey) {
+          const signedUrl = await fetchSignedUrlForImage(imageKey);
+          if (signedUrl) {
+            currentImageUrl = signedUrl;
+          }
+        }
       }
 
 
