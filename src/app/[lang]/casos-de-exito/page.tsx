9d1b63601b46fa42a2cb0eac48c70929b0ef8b15
Jonathan Johan Marquez Sanchez <cheka993jo@gmail.com>
2025-06-12 18:58:23 +0000

    agrega la imagen en Agentes de Ventas con IA y el nombre clave de la ima

--- src/app/[lang]/casos-de-exito/page.tsx
+++ src/app/[lang]/casos-de-exito/page.tsx
@@ -106,6 +106,14 @@ const getProjects = async (dscp: Dictionary['successCasesPage']): Promise<Projec
             currentImageUrl = signedUrl;
           }
         }
+      } else if (p.id === "6") {
+        const imageKey = process.env.NEXT_PUBLIC_SUCCESS_CASE_AI_SALES_IMAGE_KEY;
+        if (imageKey) {
+          const signedUrl = await fetchSignedUrlForImage(imageKey);
+          if (signedUrl) {
+            currentImageUrl = signedUrl;
+          }
+        }
       }
 
 
